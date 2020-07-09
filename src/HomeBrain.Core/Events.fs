8f349904fa1218cba99bbeb5cca32fe71cdac0e5
Bonjune <malloc099@gmail.com>
2020-07-10 00:02:43 +0900

    Update Events.fs

--- src/HomeBrain.Core/Events.fs
+++ src/HomeBrain.Core/Events.fs
@@ -2,10 +2,10 @@ module Events
 open Domain
 
 type Event =
-  | RoomCreated of Room
   | ExamStarted of Room
   | ExamEnded of Room
   | UserEntered of User * Room
   | UserExited of User * Room
   | PaperSubmitted of User * Submission
-  | MessageSent of User * User * string
+  | MessageSent of User * User * Message 
+  | RoomClosed of Room
