77daa00c993f7102fc2f6115f00901e3e7ff48b0
Bonjune <malloc099@gmail.com>
2020-07-10 00:01:37 +0900

    Update Errors.fs: add errors

--- src/HomeBrain.Core/Errors.fs
+++ src/HomeBrain.Core/Errors.fs
@@ -4,9 +4,11 @@ type Error =
   | StringTooLong of string
   | CannotSubmitPaper // after exam ends
   | CannotOpenPaper // before exam starts 
+  | CannotCloseRoomDuringExam
   | ExamAlreadyStarted
   | ExamAlreadyEnded
   | ExamNotStarted
+  | NotValidRoom
 
 let toString = function
   | StringTooLong s -> sprintf "Name is too long: %s" s
