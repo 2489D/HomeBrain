8017f7ef5da9c21cee94421b5c85c8a58970d53f
Bonjune <malloc099@gmail.com>
2020-07-13 15:11:58 +0900

    Create HomeBrain.App project

--- src/HomeBrain.App/Program.fs
+++ src/HomeBrain.App/Program.fs
@@ -1,8 +1,19 @@
 ﻿// Learn more about F# at http://fsharp.org
 
 open System
+open System.Threading
+open Suave
+open Suave.Filters
+open Suave.Operators
+open Suave.Successful
 
 [<EntryPoint>]
 let main argv =
-    printfn "Hello World from F#!"
+    let app =
+        choose
+            [   GET >=> choose
+                    [ path "/hello" >=> OK "Hello GET" ]
+                POST >=> choose
+                    [ path "/hello" >=> OK "Hello POST" ] ]
+    startWebServer defaultConfig app
     0 // return an integer exit code
