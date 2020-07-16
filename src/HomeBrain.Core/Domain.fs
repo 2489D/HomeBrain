75898ffb7a0b172ada93dad36054f7f34ca1a220
Bonjune <malloc099@gmail.com>
2020-07-16 23:05:17 +0900

    Update: elaborate User data structrue and Messages
    1. Introduce StudentData and HostData
    2. Extract Submitter data from Submission type to new Student type
    3. Elaborate Message type

--- src/HomeBrain.Core/Domain.fs
+++ src/HomeBrain.Core/Domain.fs
@@ -8,43 +8,75 @@ type File =
   | PDF
   | Essay
 
-type Person = {
-  Id: Guid
+[<NoEquality; NoComparison>]
+type HostData = {
   Name: Name20
 }
 
-type User =
-  | Student of Person
-  | Host of Person
+type StudentId = IdString of string
 
+type StudentData = {
+  Name: Name20
+  StdId: StudentId
+}
+
+[<NoEquality; NoComparison>]
 type Submission = {
-  Id: Guid
-  Student: User 
   Files: File list
   Date: DateTime
 }
 
 type String40 = String40 of string
 
+type Student = StudentData * Submission list
+type Host = HostData
+
 // Shares server time
 type Room = {
   Id: Guid
   Title: String40
-  Hosts: User list
-  Students: User list
+  Hosts: Student list
+  Students: Host list
 }
 
 type String300 = String300 of string
 
-type Message = String300
+type User =
+  | Student of Student
+  | Host of Host
 
-let tryName s =
-  if s |> String.length > 20 then None
-  else Some s
+type RequestFn = unit -> unit // FIXME
 
-let user name =
-  tryName name
-  |> Option.map (fun n -> {
-    Id = Guid.NewGuid ();
-    Name = Name20 n;
-  })
+type MessageContent =
+  | String300
+  | RequestFn
+
+type MessageFromStudentToHost = {
+  Sender: Student
+  Receivers: Host list
+  Content: MessageContent
+}
+
+type MessageFromHostToStudent = {
+  Sender: Host
+  Receivers: Student list
+  Content: MessageContent
+}
+
+type MessageFromHostToHost = {
+  Sender: Host
+  Receivers: Host list
+  Content: MessageContent
+}
+
+type Notice = {
+  Sender: Host
+  Receiver: User list
+  Content: MessageContent
+}
+
+type Message =
+  | MessageFromStudentToHost of MessageFromStudentToHost
+  | MessageFromHostToStudent of MessageFromHostToStudent
+  | MessageFromHostToHost of MessageFromHostToHost
+  | Notice of Notice
