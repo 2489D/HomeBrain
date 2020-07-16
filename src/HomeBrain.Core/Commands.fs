09a3b2e97266dd97a13d58a6fd6ffe99d0d95429
Bonjune <malloc099@gmail.com>
2020-07-16 16:30:42 +0900

    Update: have events and commands contain full context

--- src/HomeBrain.Core/Commands.fs
+++ src/HomeBrain.Core/Commands.fs
@@ -1,11 +1,17 @@
 module HomeBrain.Commands
 
+open System
 open Domain
 
+/// Commands that exposed to APIs
+/// A command should contain all contexts
+/// as a contructor parameters
+/// Please refer to Events.fs
 type Command =
   | StartExam of Room
-  | EndExam of Room
-  | EnterRoom of User * Room
-  | ExitRoom of User * Room
-  | SubmitPaper of User * Submission
-  | SendRequest of User * User array * Message 
+  | EnterRoom of Guid * User
+  | ExitRoom of Guid * User
+  | SubmitPaper of Guid * User * Submission
+  | SendMessage of User * User array * Message 
+  | EndExam of Guid 
+  | CloseRoom of Guid
--- src/HomeBrain.Core/Events.fs
+++ src/HomeBrain.Core/Events.fs
@@ -1,12 +1,17 @@
 module HomeBrain.Events
 
+open System
 open Domain
 
+/// represents all events possible to happen
+/// through an program running
+/// An event data should contain all contexts
+/// as parameters of a constructor
 type Event =
-  | ExamStarted
-  | ExamEnded
-  | UserEntered of User
-  | UserExited of User
-  | PaperSubmitted of User * Submission
-  | MessageSent of User * User * Message 
-  | RoomClosed
+  | ExamStarted of Guid
+  | UserEntered of Guid * User
+  | UserExited of Guid * User
+  | PaperSubmitted of Guid * User * Submission
+  | MessageSent of User * User array * Message 
+  | ExamEnded of Guid
+  | RoomClosed of Guid
